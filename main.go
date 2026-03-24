0674ad193bf6ea48a7b34683f6e0ad050d546214
ranggawaridat <ranggawaridat@gmail.com>
2026-03-24 21:28:31 +0700

    feat: add /ping and /users endpoints with JSON response

--- main.go
+++ main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 func main() {
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Test"))
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
 	})
 
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/users", getUsersHandler)
+
 	fmt.Println("Server running on :8080")
+	http.ListenAndServe(":8080", nil)
+}
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func getUsersHandler(w http.ResponseWriter, r *http.Request) {
+	users := []User{
+		{ID: 1, Name: "Rangga"},
+		{ID: 2, Name: "Waridat"},
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(users); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
